b1351dda376407703fdd6220a2454480d891af88
Dominik Finkbeiner <dominik.finkbeiner@sap.com>
2021-07-02 14:48:46 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    // const githubToken: string = core.getInput('githubToken')
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    core.info(payload)
+    // console.log(`The event payload: ${payload}`);
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
   } catch (error) {
     core.setFailed(error.message)
   }
